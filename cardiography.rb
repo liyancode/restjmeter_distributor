2b260be1b6553a44ec0fe2935efbd2259688b8c9
liyancode <yanli6@ebay.com>
2017-02-09 18:06:26 +0800

    core code of balancer

--- cardiography.rb
+++ cardiography.rb
@@ -1,2 +1,83 @@
 # cardiography.rb
-# agents info/status collector by heart beat
+# agents info/status collector by heart beat daemon
+
+AgentAddresses=CONFIG["RESTJmeter_Agents"]
+
+# sync lock for sync operation
+mutex=Mutex.new
+
+# {"****"=>{:address=>"****",:status=>1,..},}
+agents=Hash.new
+
+# daemon thread
+Thread.new{
+  while true
+    AgentAddresses.each{|address|
+      begin
+        client=HTTPClient.new
+        size=JSON.parse(client.get("http://#{address}/rest/hello",{},{}).content)["queue_size"].to_i
+        mutex.synchronize{
+          agents[address]={:address=>address,:status=>size}
+        }
+      rescue Exception
+        mutex.synchronize{
+          agents[address]={:address=>address,:status=>-1}
+        }
+      end
+    }
+    sleep(CONFIG["Heartbeat_Interval"]) # heartbeat interval 1 sec
+  end
+}
+
+def choose_best_agent(agents,mutex)
+  begin
+    address=""
+    mutex.synchronize{
+      address=agents.first[1][:address]
+      status=agents.first[1][:status]
+      agents.each{|agent|
+        if agent[1][:status]!=-1&&agent[1][:status]<status
+          address=agent[1][:address]
+          status=agent[1][:status]
+        elsif status==-1
+          address=agent[1][:address]
+          status=agent[1][:status]
+        end
+      }
+    }
+    return address
+  rescue Exception
+    return nil
+  end
+end
+
+post '/rest/jmx' do
+  if request.env["HTTP_X_RESTJMETER_TOKEN"]!=CONFIG["X_RESTJmeter_TOKEN"]
+    LOGGER.info("Access log. Request with invalid HTTP_X_RESTJMETER_TOKEN:#{request.env["HTTP_X_RESTJMETER_TOKEN"]}")
+    status 403
+    '{error:"X_RESTJmeter_TOKEN incorrect"}'
+  else
+    body_str=request.body.string
+    LOGGER.info("Access log. Request body:#{body_str}")
+    begin
+      best_address=choose_best_agent(agents,mutex)
+      if agents[best_address][:status]==-1
+        status 500
+        return '{error:"no available test agent!"}'
+      else
+        client=HTTPClient.new
+        url="http://#{best_address}/rest/jmx"
+        response=client.post(url, body_str, {"X_RESTJmeter_TOKEN" => CONFIG["X_RESTJmeter_TOKEN"]})
+        status 202
+        p test_id=JSON.parse(response.content)["test_id"]
+      end
+    rescue Exception=>e
+      p e
+      p "Incorrect body:#{body_str}"
+      LOGGER.error("Access log. Incorrect Request body:#{body_str}")
+      LOGGER.error("Access log. Incorrect Request body exception:#{e}")
+      status 400
+      '{error:"body format incorrect"}'
+    end
+  end
+end
